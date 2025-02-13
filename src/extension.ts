175f5d47dcd6cb4903945788e6da9aa5654b9f75
Uriah <codeitlikemiley@gmail.com>
2025-02-13 22:54:43 +0800

    hide panel on startup

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,21 @@
 import * as vscode from 'vscode';
-import { exec } from 'child_process';
-import * as os from 'os';
-import * as path from 'path';
-import * as fs from 'fs';
+import { exec } from 'node:child_process';
+import * as os from 'node:os';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
 import { updateCargoFeatures, updateProcMacroIgnored, updateRustfmtCommand } from './rust-analyzer';
 
 export function activate(context: vscode.ExtensionContext) {
   const channel = vscode.window.createOutputChannel('leptos-fmt');
-  channel.show(true);
+  channel.hide();
   context.subscriptions.push(channel);
 
   const rustAnalyzerInstalled = vscode.extensions.getExtension('rust-lang.rust-analyzer');
   if (rustAnalyzerInstalled) {
-    let message = 'Rust Analyzer (rust-lang.rust-analyzer) is installed and active.';
+    const message = 'Rust Analyzer (rust-lang.rust-analyzer) is installed and active.';
     channel.appendLine(message);
   } else {
-    let message = 'Rust Analyzer (rust-lang.rust-analyzer) is not installed.';
+    const message = 'Rust Analyzer (rust-lang.rust-analyzer) is not installed.';
     vscode.window.showErrorMessage(message);
     channel.appendLine(message);
     return;
@@ -26,24 +26,24 @@ export function activate(context: vscode.ExtensionContext) {
 
   const customLeptosfmtPath = leptosConfig.get<string>('path');
   if (customLeptosfmtPath) {
-    channel.appendLine('Custom leptosfmt path is set to ' + customLeptosfmtPath);
+    channel.appendLine(`Custom leptosfmt path is set to ${customLeptosfmtPath}`);
   }
 
   const customCargoHome = leptosConfig.get<string>('cargoHome') || '';
   if (customCargoHome) {
-    channel.appendLine('Custom cargo home is set to: ' + customCargoHome);
+    channel.appendLine(`Custom cargo home is set to: ${customCargoHome}`);
   }
 
   const defaultCargoHome = process.env.CARGO_HOME || path.resolve(os.homedir(), '.cargo');
-  channel.appendLine('Default cargo home is set to: ' + defaultCargoHome);
+  channel.appendLine(`Default cargo home is set to: ${defaultCargoHome}`);
 
   const cargoHome = customCargoHome || defaultCargoHome;
-  channel.appendLine('Cargo home being used is: ' + cargoHome);
+  channel.appendLine(`Cargo home being used is: ${cargoHome}`);
   const leptosfmtPath = customLeptosfmtPath || path.join(cargoHome, 'bin', 'leptosfmt');
-  channel.appendLine('leptosfmt path being used is: ' + leptosfmtPath);
+  channel.appendLine(`leptosfmt path being used is: ${leptosfmtPath}`);
 
   if (!fs.existsSync(leptosfmtPath)) {
-    let message = `Leptosfmt not found at ${leptosfmtPath}. Please install it or specify the correct path.`;
+    const message = `Leptosfmt not found at ${leptosfmtPath}. Please install it or specify the correct path.`;
     channel.appendLine(message);
     vscode.window.showErrorMessage(message);
     return;
@@ -54,7 +54,7 @@ export function activate(context: vscode.ExtensionContext) {
   const formatter = vscode.commands.registerCommand('leptos-fmt.format', async (document?: vscode.TextDocument) => {
     const activeDocument = document || vscode.window.activeTextEditor?.document;
     if (!activeDocument) {
-      let message = 'No active document found to format.';
+      const message = 'No active document found to format.';
       channel.appendLine(message);
       vscode.window.showErrorMessage(message);
       return;
@@ -75,12 +75,12 @@ export function activate(context: vscode.ExtensionContext) {
           }
         });
       } catch (err) {
-        let message = `Error formatting document: ${err}`;
+        const message = `Error formatting document: ${err}`;
         channel.appendLine(message);
         vscode.window.showErrorMessage(message);
       }
     } else {
-      let message = 'Only Rust files can be formatted with Leptosfmt.';
+      const message = 'Only Rust files can be formatted with Leptosfmt.';
       channel.appendLine(message);
       vscode.window.showErrorMessage(message);
     }
